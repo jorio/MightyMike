e12b257fe132870524571f1086eafaf0dba86a2f
Iliyas Jorio <iliyas@jor.io>
2022-09-09 19:55:25 +0200

    Separate OSXPPC default prefs

--- src/Heart/Main.c
+++ src/Heart/Main.c
@@ -1256,12 +1256,19 @@ static void InitDefaultPrefs(void)
 	snprintf(gGamePrefs.magic, sizeof(gGamePrefs.magic), "%s", PREFS_MAGIC);
 	gGamePrefs.interlaceMode = false;
 	gGamePrefs.difficulty = DIFFICULTY_NORMAL;
+
+#if !OSXPPC
 	gGamePrefs.pfSize = PFSIZE_WIDE;
 	gGamePrefs.displayMode = kDisplayMode_FullscreenStretched;
+	gGamePrefs.filterDithering = true;
+#else
+	gGamePrefs.pfSize = PFSIZE_MEDIUM;
+	gGamePrefs.displayMode = kDisplayMode_FullscreenCrisp;
+	gGamePrefs.filterDithering = false;
+#endif
 	gGamePrefs.windowedZoom = 0;	// 0 == automatic
 	gGamePrefs.preferredDisplay = 0;
 	gGamePrefs.uncappedFramerate = true;
-	gGamePrefs.filterDithering = true;
 	gGamePrefs.music = true;
 	gGamePrefs.interpolateAudio = true;
 	gGamePrefs.gameTitlePowerPete = false;
